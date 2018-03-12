79cf3ec3f6ef80e79f35e19e21b7b56b28dc0237
Olly John <oliver.john@faber.work>
2018-03-12 13:10:37 +0000

    Added log to say the reporter needs fixing

--- utils/report.js
+++ utils/report.js
@@ -5,15 +5,17 @@ const colours = require( 'colors' );
 
 const cwd = process.cwd();
 
-console.log( colours.dim( 'Opening HTML report file...' ) );
-opn( 'reports/html/report.html' )
-	.then( () => {
-		console.log( colours.bold.green( 'Done.' ) );
-		process.exit();
-	} )
-	.catch( () => {
-		console.log( colours.bold.red( 'Something went wrong:' ) );
-		console.log( colours.red( 
-			fs.existsSync( 'reports/html/report.html' ) ? 'Unknown error' : 'File doesn\'t exist' 
-		) );
-	} );
+// console.log( colours.dim( 'Opening HTML report file...' ) );
+// opn( 'reports/html/report.html' )
+// 	.then( () => {
+// 		console.log( colours.bold.green( 'Done.' ) );
+// 		process.exit();
+// 	} )
+// 	.catch( () => {
+// 		console.log( colours.bold.red( 'Something went wrong:' ) );
+// 		console.log( colours.red( 
+// 			fs.existsSync( 'reports/html/report.html' ) ? 'Unknown error' : 'File doesn\'t exist' 
+// 		) );
+// 	} );
+
+console.log( colours.bold.cyan( '(Reporter needs fixing)' ) );
